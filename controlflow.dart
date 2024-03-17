7e94176742a7ff187e70908cb6f76801655a618d
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-17 15:18:18 +0300

    flows

--- controlflow.dart
+++ controlflow.dart
@@ -1,7 +1,36 @@
+//  Program 3: Determine Grade Based on Marks
+// Write a Dart program to determine the grade based on a student's marks. The program should print out the appropriate grade according to the following criteria:
+// - If the marks are greater than 85, print "Excellent".
+// - If the marks are between 75 and 85 (inclusive), print "Very Good".
+// - If the marks are between 65 and 75 (inclusive), print "Good".
+// - If the marks are below 65, print "Average".
 
-## Program 3: Determine Grade Based on Marks
-Write a Dart program to determine the grade based on a student's marks. The program should print out the appropriate grade according to the following criteria:
-- If the marks are greater than 85, print "Excellent".
-- If the marks are between 75 and 85 (inclusive), print "Very Good".
-- If the marks are between 65 and 75 (inclusive), print "Good".
-- If the marks are below 65, print "Average".
+import 'dart:io';
+
+void main() {
+
+    print("Enter your marks: ");
+    String? input = stdin.readLineSync();
+
+    if (input != null) {
+        var marks = double.parse(input);
+
+
+    if (marks > 85) {
+        print("Excellent");
+    }
+    else if (marks >= 75 && marks <= 85)
+    {
+        print("Very Good");
+    }
+    else if (marks >= 65 && marks < 75)
+    {
+        print("Good");
+    }
+    else {
+    print("Average");
+ }
+} else {
+    print("no input received");
+}
+}
