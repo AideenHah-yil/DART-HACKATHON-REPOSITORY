86b675a99408252b94cbf083ac70a546388132af
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-17 15:17:44 +0300

    functions

--- functions.dart
+++ functions.dart
@@ -1,2 +1,23 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+// Program 2: Perform Mathematical Operations with Functions
+// Write a Dart program that performs two mathematical operations using functions.
+
+int add(int a, int b) {
+    int sum = a + b;
+    return sum;
+}
+
+int multiply(int a, int b) {
+    int product = a * b;
+    return product;
+}
+
+void main() {
+    int a= 5;
+    int b = 10;
+    int sum = add(a, b);
+
+    int product = multiply(a, b);
+
+    print(sum);
+    print(product);
+}
