02b6213bdd99dd4f9d990039d1e90768734b6cc0
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-17 15:17:04 +0300

    initial commit

--- program1..dart
+++ program1..dart
@@ -1,2 +1,22 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+// Program 1: Display Personal Information
+// Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+
+class Personal {
+    String name;
+    int age;
+    String school;
+    String hobby;
+
+    Personal(this.name, this.age, this.school, this.hobby);
+
+    void display() {
+        print("My name is $name, I am $age years old, I study in $school and I like $hobby.");
+        return;
+    }
+}
+
+void main() {
+    var personal = new Personal('Megumi Fushiguro', 17, 'Jujutsu Tech', 'painting');
+
+    personal.display();
+}
